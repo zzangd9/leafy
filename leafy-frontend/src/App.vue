90a3f8e04e4d8753c611a165796600b6663cc13f
zzangd9 <zzangd9@gmail.com>
2024-11-30 17:41:37 +0900

    #공백 추가

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
       <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
-      <p class="description">어제는 즐겁게 식물 관리 하셨나요?</p>
+      <p class="description">어제는 즐겁게 식물 관리 하셨나요? </p>
     </div>
     <div class="brand" v-if="isLoggedIn">
       LEAFY
